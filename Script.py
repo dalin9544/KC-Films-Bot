4d37ed484002e05dcc33197d5f8c4df6004504d0
dalin9544 <108016806+dalin9544@users.noreply.github.com>
2022-06-29 23:04:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -51,8 +51,8 @@ Mʏ ɴᴀᴍᴇ ɪꜱ <a href=https://t.me/{}>{}</a>, I ᴀᴍ ᴊᴜsᴛ ᴀɴ 
 
 <b>╔══ 𝘑𝘰𝘪𝘯 ★ 𝘚𝘩𝘢𝘳𝘦 ★ 𝘚𝘶𝘱𝘱𝘰𝘳𝘵 ══╗
 ♻️ ᴊᴏɪɴ :- <a href=https://t.me/+GH30CVjU521iZWE1><b>💠 Files Channel 💠</b></a>
-♻️ ᴊᴏɪɴ :- <a href=https://t.me/Hd_moviesmalyalam><b>🔰 Main Group 🔰</b></a>
-♻️ ᴊᴏɪɴ :- <a href=https://t.me/MoVieeBazzar><b>🧲 Main Channel🧲</b></a>
+♻️ ᴊᴏɪɴ :- <a href=https://t.me/MWM_Films><b>🔰 Main Group 🔰</b></a>
+♻️ ᴊᴏɪɴ :- <a href=https://t.me/MWMFilms><b>🧲 Main Channel🧲</b></a>
 ╚══ 𝘑𝘰𝘪𝘯 ★ 𝘚𝘩𝘢𝘳𝘦 ★ 𝘚𝘶𝘱𝘱𝘰𝘳𝘵 ══╝</b>
 
 <b>💘 Devoloper ➜ <a href=https://t.me/NJaNu_yir>💫 Njan Uyir💫</a>\n✯ ━━━━━ ✧ ━━━━━ ✯</b>\n"""
